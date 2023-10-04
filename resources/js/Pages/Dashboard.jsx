85ea1b4453b64292b71f91c287b8105918db01ef
hikachuuuxd <hikarimizu1111@gmail.com>
2023-10-04 19:04:01 +0700

    diselamatkkan

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,14 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-
-export default function Dashboard({ auth }) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+export default function Dashboard()
+{
+    return(
+        
+        <>
+        <Head title='Jurnal PKL'/>
+        <div className='w-full h-12 bg-gradient-to-r from-[#6867AC] to-[#CE7BB0] shadow-lg flex items-center'>
+            <div className='' style="background-image: url(assets/icons/LOGO SMK 1.png)"> </div>
+            <h4 className='text-white px-2  tracking-widest '> Jurnal Skensa</h4>
+        </div>
+        </>
+    )
 }
